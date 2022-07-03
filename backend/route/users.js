d6535f2a58f6ba865498cd9300d09bc3884329c1
supunvindula <e16377@eng.pdn.ac.lk>
2022-07-03 11:08:11 +0530

    added login function

--- backend/route/users.js
+++ backend/route/users.js
@@ -4,5 +4,6 @@ const userController = require("../Controllers/user.controller");
 const router = express.Router();
 
 router.post("/sign-up", userController.signup);
+router.post("/log-in", userController.login);
 
 module.exports = router;
