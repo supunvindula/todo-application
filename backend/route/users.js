d98963affad0af9862110a7e597343f845709f51
Supun Vindula <59873941+supunvindula@users.noreply.github.com>
2022-07-05 08:38:45 +0530

    Deleted backend directory because it was renamed

--- backend/route/users.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const userController = require("../Controllers/user.controller");
-
-const router = express.Router();
-
-router.post("/sign-up", userController.signup);
-router.post("/log-in", userController.login);
-
-module.exports = router;
